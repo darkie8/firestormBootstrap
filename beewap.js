ccdbe7d9c4b3739b24e414c5bf10aa91b5c369e1
darkie9 <sdarkei9@gmail.com>
2018-05-17 06:37:47 +0530

    new

--- beewap.js
+++ beewap.js
@@ -1,4 +1,65 @@
-var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
+var contJSON= {
+	"contact": [{
+			"name": "Android1",
+            "number": "#####",
+            "last_seen": "last seen 2.05 am",
+            "time": "3.09 am"
+            
+        },
+		{
+			"name": "Android2",
+            "number": "#####",
+            "last_seen": "last seen 1.00 am",
+            "time": "today"
+
+        },
+		{
+			"name": "Android3",
+            "number": "#####",
+            "last_seen": "last seen yeesterday 2.05 pm",
+            "time": "yesterday"
+            
+		},
+		{
+			"name": "Android4",
+            "number": "#####",
+            "last_seen": "last seen 4.00 am",
+            "time": "03/05/18"
+
+        },
+		{
+			"name": "Android5",
+            "number": "#####",
+            "last_seen": "last seen 03/05/18",
+            "time": "03/05/18"
+
+        },
+		{
+			"name": "Android6",
+            "number": "#####",
+            "last_seen": "last seen 1.00 am",
+            "time": "05/05/18"
+
+        },
+		{
+			"name": "Android7",
+            "number": "#####",
+            "last_seen": "last seen 9.00 am",
+            "time": "08/02/18"
+
+        },
+		{
+			"name": "Android8",
+            "number": "#####",
+            "last_seen": "last seen 10.00 am",
+            "time": "04/01/18"
+
+        }
+	],
+	"readChoice": ["Mark as unread", "Mark as read"]
+}
+
+// declared an array
     let p = [];
      let a = []
      let A;
@@ -8,10 +69,10 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
      for(k=0;k<8;k++){
          a.push(true)
          if(a[k]==true){
-            A= readChoice[0]; // if array element is true 
+            A= contJSON.readChoice[0]; // if array element is true 
          }
          else{
-            A= readChoice[1];// if array elemnt is false
+            A= contJSON.readChoice[1];// if array elemnt is false
          }
      }
      console.log(a) 
@@ -28,14 +89,14 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
     <div class="col-12 d-flex px-2 flex-row align-items-center py-3 kiop gama drooid" id="pix${i}">
         
         <div class="py-3 px-3 rounded-circle picture${i} lambda"></div>
-        <p class="text-left my-auto Awrt px-2 pl-3 sailormoon">Get Notified of New Messages<br>
+        <p class="text-left my-auto Awrt px-2 pl-3 sailormoon">${contJSON.contact[i-1].name}<br>
             <small class="text-left ASwrt sailormoon1">Turn On Desktop Notification</small></p>
-            <div class="pix${i} ml-auto le"><small id="df">yesterday</small></div>
+            <div class="pix${i} ml-auto le"><small id="df">${contJSON.contact[i-1].time}</small></div>
     </div>`);
     $(`.picture${i}`).css(`background`,`url(static/A${i}.jpg)`);
     // add certain html in pix${i} class
     $(`.pix${i}`).append(`
-    <div class="dropdown d-flex justify-content-center be">
+    <div class="dropdown ken d-flex justify-content-center be">
     <button type="button" class="btn btn-link  pr-1" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fas fa-angle-down"></i>
         </button>
@@ -46,7 +107,7 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
         <a class="dropdown-item" href="#" id="reUn${i}">${A}</a>
       </div>
     </div> `);
-    $(`.dropdown`).on({
+    $(`.ken`).on({
          click: function(){ n= true}, // if dropdown is clicked..n=true
          mouseleave: function(){n= false} // if mouse leaves dropdown n=false
     });
@@ -58,15 +119,15 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
              
         $(`#pix${i}`).addClass("dreco").addClass("shadow");
              a[i-1]= false
-             A= readChoice[1];
-             $(`#reUn${i}`).html(readChoice[1]);
+             A= contJSON.readChoice[1];
+             $(`#reUn${i}`).html(contJSON.readChoice[1]);
         }
         // #pix${i} container will be higlighted and #reUn${i} container will be changed to read 
         else {
                  
                 $(`#pix${i}`).removeClass("dreco").removeClass("shadow");
                 a[i-1]= true
-                $(`#reUn${i}`).html(readChoice[0]);
+                $(`#reUn${i}`).html(contJSON.readChoice[0]);
         }
         console.log(a)
         console.log(n)
@@ -84,12 +145,21 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
         if(a[i-1]==false){
             $(this).removeClass("dreco").removeClass("shadow");
             a[i-1]= true
-            $(`#reUn${i}`).html(readChoice[0]);
+            $(`#reUn${i}`).html(contJSON.readChoice[0]);
             console.log(a)
         }
          
         
-        $(".zeta").attr("src", `static/A${i}.jpg`); // picture of the contact uploaded in the zeta class container
+        $(".zeta,.zeta1").attr("src", `static/A${i}.jpg`); // picture of the contact uploaded in the zeta class container
+        $(`.zeta`).click(function () { 
+            $(".zeta1").attr("src", `static/A${i}.jpg`);
+            $("#cea1").animate({width: '160',height: '160'});
+            $("#cea").animate({width: '100',height: '100'});
+            
+        });
+        $(`.contName`).text(`${contJSON.contact[i-1].name}`);
+        $(`.number`).text(`${contJSON.contact[i-1].number}`);
+        $(`.lstSeen`).text(`${contJSON.contact[i-1].last_seen}`);
         $(`.set`).empty(); // to empty the prev loaded convo
 
      // making random chat conversation 
@@ -109,10 +179,10 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
 
     // disapearing and reapearing the dropdown class icon
     $(`#pix${i}`).hover(function () {
-        $(this).find(".dropdown").removeClass("be");
+        $(this).find(".ken").removeClass("be");
         
     }, function () {
-        $(this).find(".dropdown").addClass("be");
+        $(this).find(".ken").addClass("be");
     }
     ); // function calling ends
     } // for loop ends
@@ -124,7 +194,7 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
     a.splice(5,1,false);
     console.log(a)
     $(`#pix1,#pix3,#pix6`).addClass("dreco").addClass("shadow")
-    $(`#reUn1,#reUn3,#reUn6`).html(readChoice[1])}
+    $(`#reUn1,#reUn3,#reUn6`).html(contJSON.readChoice[1])}
     
     // calling function to see unseen effect
     unseenInbox();
@@ -146,6 +216,15 @@ var readChoice= [`Mark as unread`,'Mark as read'] // declared an array
         $(".noob").animate({width: '0%'});
         $(".jig").animate({width: '0',height: '0'});
         $(".mig").animate({width: '0',height: '0'});
+    });
+    $(`.zeta`).click(function () { 
+        $(".io").animate({width: '60%'});
+        
+    });
+    $(`.jigsaw`).click(function () { 
+        $(".io").animate({width: '100%'});
+        $("#cea1").animate({width: '0',height: '0'});
+        $("#cea").animate({width: '0',height: '0'});
     });
     }); 
 
